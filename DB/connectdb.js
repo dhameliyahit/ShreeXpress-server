6e05e33cbcc82b3b91e09a6adb64dab89f3e1535
dhameliyahit <heetdhameliya59@gmail.com>
2025-07-13 00:31:27 +0530

    fix for hosting

--- DB/connectdb.js
+++ DB/connectdb.js
@@ -2,12 +2,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-    rejectUnauthorized: false,
-  },
+const isProduction = process.env.NODE_ENV === 'production';
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 
