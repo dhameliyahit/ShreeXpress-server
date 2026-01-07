68adb19c660f7e5b4919fcc0dec138b3d2df7a57
balarcrens <balarcrens@gmail.com>
2026-01-07 22:19:10 +0530

    change cors allowed origin

--- app.js
+++ app.js
@@ -22,7 +22,23 @@ const { protect, superadmin } = require("./middleware/authMiddleware");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+    "https://shreexpresscourier.netlify.app",
+    "http://localhost:5173"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
